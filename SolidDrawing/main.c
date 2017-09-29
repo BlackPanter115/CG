183b1e9d04bdb1c95aff288a69444077748e3899
BlackPanter115 <miguel.rojas115@hotmail.com>
2017-09-29 14:40:06 -0500

    Archivo main

---
+++ SolidDrawing/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "vertexListFormat.h"
+
+int main(int argc, char *argv[])
+{
+	principalVLF(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6],
+		     argv[7], argv[8], argv[9], argv[10]);
+}
