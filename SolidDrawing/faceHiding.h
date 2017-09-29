cf1143a3f410a9c72b44f421a5c8d765f8ecc7ff
BlackPanter115 <miguel.rojas115@hotmail.com>
2017-09-29 14:34:36 -0500

    Headers del archivo faceHiding.c

---
+++ SolidDrawing/faceHiding.h
@@ -0,0 +1,23 @@
+#ifndef __FACEHIDING_H__
+#define __FACEHIDING_H__
+
+struct segment {
+	double x1;
+	double y1;
+	double z1;
+	double x2;
+	double y2;
+	double z2;
+};
+
+struct vector {
+	double x;
+	double y;
+	double z;
+};
+
+double principalFH(struct segment *s1, struct segment *s2);
+struct vector *createVector(struct segment *aux);
+struct vector *createNormalVector(struct vector *vectorA,
+				  struct vector *vectorB);
+#endif
