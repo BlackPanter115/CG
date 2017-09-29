f0388a72a3f024f4bb9999886cf0085bdcf81977
BlackPanter115 <miguel.rojas115@hotmail.com>
2017-09-29 14:50:52 -0500

    Archivo main

---
+++ Vertex-List-Format/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "vertexListFormat.h"
+
+int
+main (int argc, char *argv[])
+{
+  principalVLF (argv[1], argv[2], argv[3], argv[4], argv[5], argv[6],
+		argv[7]);
+}
